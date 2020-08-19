eb7c2412ee32f58b716972e6abfbd989f8e72a32
elfcoda <nanohnu@gmail.com>
2020-08-19 15:48:42 +0800

    update tmp

--- lvalue.cpp
+++ lvalue.cpp
@@ -1,5 +1,27 @@
-#include <iostream>
+#include "in.h"
 using namespace std;
+
+template <typename T>
+constexpr bool is_vector = std::is_same_v<typename std::decay<T>::type, std::vector<typename std::decay<T>::type::value_type, typename std::decay<T>::type::allocator_type>>;
+
+template <typename T>
+struct is_container_vector_helper
+{
+    constexpr static bool is_vector = false;
+};
+template <typename... U>
+struct is_container_vector_helper<std::vector<U...>>
+{
+    constexpr static bool is_vector = true;
+};
+
+template <typename T>
+struct is_container_vector
+{
+    constexpr static bool is_vector = is_container_vector_helper<typename std::decay<T>::type>::is_vector;
+};
+
+
 
 void f1(int& x)
 {
@@ -11,9 +33,14 @@ void f1(int&& x)
     cout << "rrrrr" << endl;
 }
 
+template <class T>
+std::enable_if_t<is_container_vector<T>::is_vector, void> h1(T&& v1)
+{
+}
+
 int main()
 {
-    int i = 9;
-    f1(i);
+    std::vector<int> v = {1};
+    h1(v);
     return 0;
 }
